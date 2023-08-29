b29d91c009bc6eed913fcb29e9f4112c85402bdd
tommy-mitchell <tmitchell7@uh.edu>
2023-08-29 00:10:40 -0500

    feat: update template

--- template/test/_utils.ts
+++ template/test/_utils.ts
@@ -1,6 +1,3 @@
 import { fileURLToPath } from "node:url";
-import path from "node:path";
 
-export const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-export const atFixture = (name: string) => path.join(__dirname, "fixtures", name);
+export const atFixture = (name: string) => fileURLToPath(new URL(`_fixtures/${name}`, import.meta.url));
--- template/test/test.ts
+++ template/test/test.ts
@@ -1,6 +1,6 @@
 import test from "ava";
-import {{ tmplr.function_name }} from "../src/index.js";
-import { __dirname, atFixture } from "./_utils.js";
+import {{ tmplr.function_name }} from "~/index.js";
+import { atFixture } from "./_utils.js";
 
 test("main", t => {
 	t.is({{ tmplr.function_name }}("hello"), "hello world");
